f0f5dea11fcc3286476e074bbc9a32f55595e4e7
ycetin94 <ycetin94@gmail.com>
2022-12-27 09:09:51 +0300

    project files added

---
+++ mern-files/frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://backend-app:5000',
+      changeOrigin: true,
+    })
+  );
+};
